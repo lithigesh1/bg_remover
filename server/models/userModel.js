af74fc9777cb7d6b98e363478ca608cf69411c94
lithigesh1 <lithigesh@gmail.com>
2025-05-23 21:01:43 +0530

    webhooks added

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    clerID : {type: String, required: true, unique: true},
+    clerkID : {type: String, required: true, unique: true},
     email : {type: String, required: true, unique: true},
     photo : {type: String, required: true},
     firstname: {type:String},
